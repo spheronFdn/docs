baef9f27bd705d3b4868921d1e6ebb77758793a4
aayushmahapatra <mahapatraaayush@gmail.com>
2023-03-28 12:44:04 +0530

    Add unified section for upgrading to pro

---
+++ components/upgrade-card.tsx
@@ -0,0 +1,30 @@
+import { FC, ReactElement } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./upgrade.module.css";
+
+export interface IUpgradeCard {
+  path: string;
+  logo: any;
+  alt: string;
+  title: string;
+}
+
+const UpgradeCard: FC<IUpgradeCard> = ({ path, logo, alt, title }) => {
+  return (
+    <Link href={`${path}`} className={styles.card}>
+      <div>
+        <Image
+          src={logo}
+          alt={alt}
+          width={24}
+          height={24}
+          className={styles.logo}
+        />
+        <h3 className={styles.heading}>{title}</h3>
+      </div>
+    </Link>
+  );
+};
+
+export default UpgradeCard;
