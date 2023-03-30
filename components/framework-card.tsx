a41cb2491e22bbfc9d6535815c9cf335a035e762
aayushmahapatra <mahapatraaayush@gmail.com>
2023-03-30 14:37:18 +0530

    Add icons in framework guide

--- components/framework-card.tsx
+++ components/framework-card.tsx
@@ -1,16 +1,28 @@
 import { FC } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/framework.module.css";
 
 export interface IFrameworkCard {
   path: string;
+  logo?: any;
+  alt?: string;
   title: string;
 }
 
-const FrameworkCard: FC<IFrameworkCard> = ({ path, title }) => {
+const FrameworkCard: FC<IFrameworkCard> = ({ path, logo, alt, title }) => {
   return (
     <Link href={`${path}`} className={styles.card}>
       <div>
+        {logo && (
+          <Image
+            src={logo}
+            alt={alt}
+            width={24}
+            height={24}
+            className={styles.logo}
+          />
+        )}
         <h3 className={styles.heading}>{title}</h3>
       </div>
     </Link>
