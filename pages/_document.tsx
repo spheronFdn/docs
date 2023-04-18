c30c350c75040df49068a8bdb4192766ae6b9dc4
Abhinav Gaur <abhi.gaur.007@gmail.com>
2023-04-19 01:32:17 +0530

    Add new GTM tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,22 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css"
         />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
       </Head>
       <body style={{ fontFamily: "Gilroy, sans-serif", fontWeight: 600 }}>
         <Main />
