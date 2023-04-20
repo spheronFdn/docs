68949e6e3ea691f5daaef3df669e16be9a1e8358
aayushmahapatra <mahapatraaayush@gmail.com>
2023-04-20 17:58:07 +0530

    Update meta image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import SpheronLogo from "../assets/favicon.ico";
-import Spheron from "../assets/spheron.svg";
 
 const METADATA = {
-  title: "Spheron",
-  description: "Power your dapp with Spheron's next-generation infrastructure",
+  title: "Documentation - Spheron",
+  description: "Power your dapp with Spheron's next-generation infrastructure.",
   url: "https://docs.spheron.network/",
+  image: "https://spheron.network/metaimage.png",
 };
 
 export default function App({ Component, pageProps }) {
@@ -18,19 +18,19 @@ export default function App({ Component, pageProps }) {
 
         <meta itemProp="name" content={METADATA.title} />
         <meta itemProp="description" content={METADATA.description} />
-        <meta itemProp="image" content={Spheron} />
+        <meta itemProp="image" content={METADATA.image} />
 
         <meta property="og:url" content={METADATA.url} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={METADATA.title} />
         <meta property="og:description" content={METADATA.description} />
-        <meta property="og:image" content={Spheron} />
+        <meta property="og:image" content={METADATA.image} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={METADATA.title} />
         <meta name="twitter:description" content={METADATA.description} />
-        <meta name="twitter:image" content={Spheron} />
+        <meta name="twitter:image" content={METADATA.image} />
         <link rel="canonical" href={METADATA.url} />
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content={METADATA.title} />
