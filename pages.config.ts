2090977d6c7bc0138532fb502a22f1ba77010073
aayushmahapatra <mahapatraaayush@gmail.com>
2023-09-20 01:45:16 +0530

    Add upload section in storage docs

--- pages.config.ts
+++ pages.config.ts
@@ -41,6 +41,7 @@ export const Pages = [
   ["Spot", "/compute/type/spot"],
   ["On Demand", "/compute/type/on-demand"],
   ["Autoscale", "/compute/type/autoscale"],
+  ["Upload", "/storage/upload"],
   ["Dedicated Gateways", "/gateways/dedicated"],
   ["Public Gateway", "/gateways/public-gateway"],
   ["Framework Guide", "/framework-guide"],
