2b2299cf42045f9c896c889014c4a9ec64be1f76
Giri-Spheron <giri@spheron.network>
2023-10-26 00:37:23 +0530

    add copy functionality

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-});
+const withNextra = require("nextra")(
+  "nextra-theme-docs",
+  "./theme.config.tsx",
+  {
+    defaultShowCopyCode: true, // This line enables the copy button globally
+  }
+);
 
 module.exports = withNextra({
   images: {
