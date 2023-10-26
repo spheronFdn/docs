a0b804b445bc82ca66a3b7c25484cc0f0086abac
Giri-Spheron <giri@spheron.network>
2023-10-26 22:21:56 +0530

    update nextra import-structure

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const withNextra = require("nextra")(
-  "nextra-theme-docs",
-  "./theme.config.tsx",
-  {
-    defaultShowCopyCode: true, // This line enables the copy button globally
-  }
-);
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+});
 
 module.exports = withNextra({
   images: {
