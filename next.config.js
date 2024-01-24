84dcbbd658bc8f9709fd600069babeb06f4b23ee
aayushmahapatra <mahapatraaayush@gmail.com>
2024-01-24 21:46:16 +0530

    Update redirect

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
+});
+
+module.exports = withNextra({
   async redirects() {
     return [
       {
-        source: "/sdk/storage-v2",
-        destination: "/sdk/storage",
+        source: "/sdk/storage-v2/",
+        destination: "/sdk/storage/",
         permanent: true,
       },
     ];
   },
-});
-
-module.exports = withNextra({
   images: {
     loader: "akamai",
     path: "",
