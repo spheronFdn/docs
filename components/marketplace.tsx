d330ba7e264aaa42371904f3446ad167801026ac
aayushmahapatra <mahapatraaayush@gmail.com>
2024-03-13 10:28:30 +0530

    Add docs for subspace node

--- components/marketplace.tsx
+++ components/marketplace.tsx
@@ -26,6 +26,7 @@ import TensorFlowIcon from "../assets/tensorflow.svg";
 import ShardeumIcon from "../assets/shardeum.svg";
 import CelestiaIcon from "../assets/celestia.svg";
 import ElixirIcon from "../assets/elixir.png";
+import SubspaceIcon from "../assets/subspace.svg";
 import styles from "../styles/framework.module.css";
 
 const MARKETPLACE_APP_LIST = [
@@ -208,6 +209,12 @@ const MARKETPLACE_APP_LIST = [
     logo: ElixirIcon,
     alt: "elixir",
     title: "Deploy Elixir Validator Node",
+  },
+  {
+    path: "/marketplace-guide/subspace",
+    logo: SubspaceIcon,
+    alt: "subspace",
+    title: "Deploy Subspace Node",
   },
 ];
 
